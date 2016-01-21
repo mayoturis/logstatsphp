ef7e315a3a785e5b89baf4434dcd3ee95b962a01
mayo505 <marek.turis@gmail.com>
2016-01-21 22:03:03 +0100

    curl returntransfer

--- src/Logstats/LogstatsHandler.php
+++ src/Logstats/LogstatsHandler.php
@@ -46,6 +46,7 @@ class LogstatsHandler extends AbstractProcessingHandler {
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_exec($ch);
 		curl_close($ch);
 	}
